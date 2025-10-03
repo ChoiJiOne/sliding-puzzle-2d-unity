afb47d2b7e0ebd703d56ee01d8fff8597037e912
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2025-10-03 10:00:56 +0900

    보드에서 타일 생성 시 세팅도 함께 하도록 수정

--- SlidingPuzzle/Assets/Scripts/Board.cs
+++ SlidingPuzzle/Assets/Scripts/Board.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Board : MonoBehaviour
 {
-    [SerializeField] private GameObject _tilePrefab;
+    [SerializeField] private Tile _tilePrefab;
     [SerializeField] private RectTransform _tilesParent;
 
     private Vector2Int _puzzleSize = new Vector2Int(4, 4);
@@ -18,7 +18,8 @@ public class Board : MonoBehaviour
         {
             for (int x = 0; x < _puzzleSize.x; ++x)
             {
-                Instantiate(_tilePrefab, _tilesParent);
+                Tile tile = Instantiate(_tilePrefab, _tilesParent);
+                tile.Setup(_puzzleSize.x * _puzzleSize.y, y * _puzzleSize.x + x + 1);
             }
         }
     }
