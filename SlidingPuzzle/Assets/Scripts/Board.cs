60c450f3824a1562491c126d08fd8dc82a0e7f9f
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2025-10-03 10:08:55 +0900

    타일을 임의로 섞는 기능 추가

--- SlidingPuzzle/Assets/Scripts/Board.cs
+++ SlidingPuzzle/Assets/Scripts/Board.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Board : MonoBehaviour
@@ -6,10 +8,12 @@ public class Board : MonoBehaviour
     [SerializeField] private RectTransform _tilesParent;
 
     private Vector2Int _puzzleSize = new Vector2Int(4, 4);
+    private List<Tile> _tileList = new();
 
     private void Awake()
     {
         SpawnTiles();
+        StartCoroutine(nameof(OnSuffle));
     }
 
     private void SpawnTiles()
@@ -19,8 +23,27 @@ public class Board : MonoBehaviour
             for (int x = 0; x < _puzzleSize.x; ++x)
             {
                 Tile tile = Instantiate(_tilePrefab, _tilesParent);
+                _tileList.Add(tile);
                 tile.Setup(_puzzleSize.x * _puzzleSize.y, y * _puzzleSize.x + x + 1);
             }
+        }
+    }
+
+    private IEnumerator OnSuffle()
+    {
+        float current = 0.0f;
+        float percent = 0.0f;
+        float time = 1.5f;
+
+        while (percent < 1.0f)
+        {
+            current += Time.deltaTime;
+            percent = current / time;
+
+            int index = Random.Range(0, _puzzleSize.x * _puzzleSize.y);
+            _tileList[index].transform.SetAsLastSibling();
+
+            yield return null;
         }
     }
 }
