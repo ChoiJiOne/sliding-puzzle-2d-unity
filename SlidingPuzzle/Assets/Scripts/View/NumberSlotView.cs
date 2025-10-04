f8acf41a78f945857667d3d27d4e539b16b409b3
CHOIJIONE\choij <choijione02@gmail.com>
2025-10-04 18:15:12 +0900

    보드 내에 숫자 슬롯 추가

---
+++ SlidingPuzzle/Assets/Scripts/View/NumberSlotView.cs
@@ -0,0 +1,12 @@
+using TMPro;
+using UnityEngine;
+
+public class NumberSlotView : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _numberText;
+    [SerializeField] private RectTransform _rectTransform;
+
+    private int _number = 0;
+
+
+}
