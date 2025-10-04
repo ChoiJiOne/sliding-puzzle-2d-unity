8ba018acb67bdc93d3b8a5ecfa7d87364dda07ac
CHOIJIONE\choij <choijione02@gmail.com>
2025-10-04 18:34:34 +0900

    보드 내 숫자 슬롯의 프로퍼티 추가

--- SlidingPuzzle/Assets/Scripts/View/NumberSlotView.cs
+++ SlidingPuzzle/Assets/Scripts/View/NumberSlotView.cs
@@ -4,21 +4,25 @@ using UnityEngine.UI;
 
 public class NumberSlotView : MonoBehaviour
 {
+    public int Number => _number;
+    public Vector3 LocalPosition => _rectTransform.localPosition;
+    public bool IsCorrected => _correctPosition == _rectTransform.localPosition;
+
     [SerializeField] private TextMeshProUGUI _numberText;
     [SerializeField] private Image _numberBgImage;
     [SerializeField] private RectTransform _rectTransform;
 
     private int _number = 0;
+    private Vector3 _correctPosition;
 
     public void Bind(int number, int hideNumber)
     {
         _number = number;
         _numberText.text = $"{number}";
+        _correctPosition = _rectTransform.localPosition;
 
-        if (number == hideNumber)
-        {
-            _numberText.enabled = false;
-            _numberBgImage.enabled = false;
-        }
+        bool isHide = (number == hideNumber);
+        _numberText.enabled = !isHide;
+        _numberBgImage.enabled = !isHide;
     }
 }
