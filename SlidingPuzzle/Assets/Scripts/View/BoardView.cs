4352bbeaf2301150f954ac4d5dbc06e36a237cbb
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2025-10-04 17:31:54 +0900

    에디터에서 보드의 Row/Col 크기를 변경할 경우 RectTransform의 크기를 즉시 변경하도록 수정

---
+++ SlidingPuzzle/Assets/Scripts/View/BoardView.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BoardView : MonoBehaviour
+{
+    [SerializeField] private GridLayoutGroup _gridLayoutGroup;
+    [SerializeField] private RectTransform _rectTransform;
+    [SerializeField, Range(3, 6)] private int _rowSize;
+    [SerializeField, Range(3, 6)] private int _colSize;
+
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        if (_gridLayoutGroup == null || _rectTransform == null)
+        {
+            Debug.Log("INVALID_BOARD_VIEW_REFERENCE");
+            return;
+        }
+
+        float width = _gridLayoutGroup.cellSize.x * _colSize + _gridLayoutGroup.spacing.x * (_colSize + 1);
+        float height = _gridLayoutGroup.cellSize.y * _rowSize + _gridLayoutGroup.spacing.y * (_rowSize + 1);
+
+        _rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, width);
+        _rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, height);
+    }
+#endif
+}
